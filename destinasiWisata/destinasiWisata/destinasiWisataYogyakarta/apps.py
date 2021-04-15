974ded792db856648477c1322c3b04bfb399caf4
aryyawijaya <aryyawijaya@gmail.com>
2021-04-15 21:49:36 +0700

    setup project (venv, database, admin)

---
+++ destinasiWisata/destinasiWisata/destinasiWisataYogyakarta/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DestinasiwisatayogyakartaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'destinasiWisataYogyakarta'
