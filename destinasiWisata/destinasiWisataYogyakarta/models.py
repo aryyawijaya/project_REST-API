5ad5ccd88b628de506402c24699112b524cb2b74
aryyawijaya <aryyawijaya@gmail.com>
2021-04-16 16:57:26 +0700

    add created_at field in models

--- destinasiWisata/destinasiWisataYogyakarta/models.py
+++ destinasiWisata/destinasiWisataYogyakarta/models.py
@@ -8,6 +8,7 @@ class Destination(models.Model):
     address = models.TextField()
     site = models.URLField()
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
