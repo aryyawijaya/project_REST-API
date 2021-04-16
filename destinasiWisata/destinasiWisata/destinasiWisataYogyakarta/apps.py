1ae3cb05611163273f030b6e853858886bd96918
Aryya Bagus P <71710840+aryyawijaya@users.noreply.github.com>
2021-04-16 10:13:46 +0700

    Delete destinasiWisata directory

--- destinasiWisata/destinasiWisata/destinasiWisataYogyakarta/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DestinasiwisatayogyakartaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'destinasiWisataYogyakarta'
